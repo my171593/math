bf84cf7050defcd64fcc7bfc4c42c0b568f96599
my171593 <85271052+my171593@users.noreply.github.com>
2021-08-10 12:27:21 -0400

    Add files via upload

--- gamepage.js
+++ gamepage.js
@@ -3,7 +3,7 @@ function adduser(){
     var player2_name=document.getElementById("player2nameinput").value;
     localStorage.setItem("player 1 name",player1_name );
     localStorage.setItem("player 2 name",player2_name ); 
-    window.location.replace("index.html");
+    window.location.replace("login.html");
 
 
 }
